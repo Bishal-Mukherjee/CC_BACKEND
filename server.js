5435e3ec002b4eccb99904cd68472ce4895cfcb7
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2023-02-07 19:45:11 +0530

    if block removed

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ mongoose
   });
 mongoose.set("strictQuery", true);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use("/api/users", require("./routes/users"));
 
